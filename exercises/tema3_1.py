d5b0a92a65cde471f5a7f1d4ce4fba6872f902ae
FerBaeza <fbaezahurtado@gmail.com>
2022-08-31 16:06:45 +0200

    ex3

--- exercises/tema3_1.py
+++ exercises/tema3_1.py
@@ -5,8 +5,18 @@ def exercise_three():
     age= input('Introduzca su edad : ')
     email= input('Introduzca su email : ')
     phone= input('Introduzca su telefono : ')
+    friends=[]
+    friends.append(input('Dime el nombre de dos amigos: '))
+    favourites_films={}
+    p=favourites_films['peliculas_vistas'] = input('Dime el nombre de las peliculas que has visto: ')
 
     print('\nLos datos introducidos son : \n\n {} {} de {} anyos de edad \n email: {} \n phone: {}\n\n'.format(name, surname, age, email, phone))
+    print('Nombre de tus amigos: ')
+    for friend in friends:
+        print(friend)
+
+    print('\nNombre de las peliculas vistas: ')
+    print(p)
 
 
 if __name__ == '__main__':
