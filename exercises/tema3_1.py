dbaa6bc17494e789a9ddaf7acf9f96e2e093b68b
FerBaeza <fbaezahurtado@gmail.com>
2022-09-01 23:15:32 +0200

    ex3.2

--- exercises/tema3_1.py
+++ exercises/tema3_1.py
@@ -16,8 +16,21 @@ def exercise_three():
         print(friend)
 
     print('\nNombre de las peliculas vistas: ')
-    print(p)
+    print(p,'\n')
 
+    
+def exercise_three_two():
+    a=5.1
+    b=a**2
+    print(b)
+    print('\nIntroduce tu peso en Kg:  ... por ejemplo 76,8 Kg')
+    print('\nIntroduce tu altuta en metros :  ... por ejemplo 1,76 m\n')
+    peso = float(input('Introduce tu peso: '))
+    altura = float(input('Introduce tu altura: '))
+    imc = float(peso/altura**2)
+    imc = "{0:.2f}".format(imc)
+    print('\nLos datos introducidos son: {} kg y una altura de {} m nTu IMC es de {}'.format(peso, altura, imc))
 
 if __name__ == '__main__':
     exercise_three()
+    exercise_three_two()
