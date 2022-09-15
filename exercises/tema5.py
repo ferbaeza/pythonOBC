2cca98774799fed9e555c7c70962c01301c0084a
FerBaeza <fbaezahurtado@gmail.com>
2022-09-15 12:29:07 +0200

    ej5

--- exercises/tema5.py
+++ exercises/tema5.py
@@ -1,7 +1,17 @@
-def first():
-    print('Ejercicio 5\n')
-
-
+def first(year):
+    if year % 4 != 0: 
+        print("No es bisiesto")
+    elif year % 4 == 0 and year % 100 != 0:
+        print("Es bisiesto")
+    elif year % 4 == 0 and year % 100 == 0 and year % 400 != 0: 
+        print("No es bisiesto")
+    elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+        print("Es bisiesto")
 
 if __name__ == '__main__':
-    first()
+    print('Ejercicio 5\n')
+
+    year = int(input('Introduce el año que tu quieras: '))
+    print('\nEl año que has seleccionado es {}. \n'.format(year))
+
+    first(year)
