36c8fd4b0b160f64dfcfa2fff099884b4d786772
FerBaeza <fbaezahurtado@gmail.com>
2022-09-15 12:12:23 +0200

    4

---
+++ exercises/tema4.py
@@ -0,0 +1,13 @@
+def first():
+    print('Ejercicio 4\n')
+
+    lista=[]
+    for i in range(1,101):
+        lista.append(i)
+    lista.reverse()
+    print(lista)
+
+
+
+if __name__ == '__main__':
+    first()
---
+++ exercises/tema5.py
@@ -0,0 +1,7 @@
+def first():
+    print('Ejercicio 5\n')
+
+
+
+if __name__ == '__main__':
+    first()
