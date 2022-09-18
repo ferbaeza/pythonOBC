b39e58641b933c39d626db5c2e16c0d02ac8d24c
FerBaeza <fbaezahurtado@gmail.com>
2022-09-18 22:34:38 +0200

    tema7.1

---
+++ exercises/tema7/main.py
@@ -0,0 +1,15 @@
+from operaciones import suma, resta, multiplicacion, division
+def main():
+    n = 'fBaeza'
+    print(f"Efercicio Tema 7, {n}\n")
+    a1= 5
+    a2= 4
+    print("El resultado de la suma es: ",suma(a1, a2))
+    print("El resultado de la resta es: ",resta(a1, a2))
+    print("El resultado de la multiplicacion es: ",multiplicacion(a1, a2))
+    print("El resultado de la division es: ",division(a1, a2))
+
+
+
+if __name__ == "__main__":
+    main()
