7a88ea3dc74170913b2cc687b606781a1199fb71
FerBaeza <fbaezahurtado@gmail.com>
2022-09-19 10:43:23 +0200

    eje8.2

--- exercises/tema8_2.py
+++ exercises/tema8_2.py
@@ -1,34 +1,56 @@
 import os
 from dataclasses import dataclass
+import pickle
 
-def first():
-    print('Ejercicio 2 Tema 8')
+class Coche():
+    name="",
+    branch=""
 
-    @dataclass
-    class Vehicle():
-        name: str
-        branch: str
+    def __init__(self, name, branch) -> None:
+        self.name=name,
+        self.branch=branch
 
-        def __str__(self) -> str:
-            return f'Your vehicle is {self.name} from {self.branch}'
-
-    v1= Vehicle('beta','alfa')
-    print(v1)
-    print('Representacion del Objeto : '+repr(v1))
-
-    
-    ruta = '/home/fer/visual/openBootCamp/python/pythonOBC/exercises/'
-    file = open(ruta+'Class.txt', 'w')
-    file.write('Fernando Baeza\n'+ os.linesep) #Salto de linea
-    file.write('Esta es la linea guardada de nuestro Objeto:'+ os.linesep) #Salto de linea
-    file.write(repr(v1))
-    file.close()
-    # Creo un variable distinta solo para leerlo
-    object = open(ruta+'Class.txt', 'r')
-    print(object.read())
-    object.close()
+    def get_full_name(self):
+        return f"Your car is a {self.branch} {self.name}"
 
 
+@dataclass
+class Vehicle():
+    name: str
+    branch: str
 
-if __name__ == '__main__':
-    first()
+    def __str__(self) -> str:
+        return f'Your vehicle is {self.name} from {self.branch}'
+
+ruta = '/home/fer/visual/openBootCamp/python/pythonOBC/exercises/'
+
+
+coche= Coche('Ibiza','Seat')
+f= open(ruta+"Class.bin", "wb")
+pickle.dump(coche, f)
+f.close()
+
+
+v1=Vehicle("beta", "alfa")
+print(v1)
+print('Representacion del Objeto : '+repr(v1))
+
+
+
+file = open(ruta+'Class.txt', 'w')
+file.write('Fernando Baeza\n'+ os.linesep) #Salto de linea
+file.write('Esta es la linea guardada de nuestro Objeto:'+ os.linesep) #Salto de linea
+file.write(repr(v1)) 
+file.close()
+
+
+
+
+old= open(ruta+"Class.bin", "rb")
+old_car= pickle.load(old)
+old.close()
+
+
+print(type(old_car))
+print(repr(old_car))
+print(old_car.get_full_name())
